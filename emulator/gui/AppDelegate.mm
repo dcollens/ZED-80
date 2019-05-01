607b032e49e62d5965a3b0db1aa7dc2ef3c77439
Lawrence Kesteloot <lk@teamten.com>
2019-04-30 21:57:23 -0700

    Move emulator logic from AppDelegate to ViewController.

--- emulator/gui/AppDelegate.mm
+++ emulator/gui/AppDelegate.mm
@@ -7,89 +7,19 @@
 //
 
 #import "AppDelegate.h"
-#import "zed-80.hpp"
-#import <fstream>
 
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::unique_ptr;
-using std::make_unique;
-using std::make_shared;
-using std::vector;
-using std::string;
-using std::ifstream;
-
-@interface AppDelegate () {
-    ZED80 _zed80;
-    NSTimer *_emulatorRunTimer;
-}
+@interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
 
-- (instancetype)init {
-    self = [super init];
-
-    _emulatorRunTimer = nil;
-
-    return self;
-}
-
-static unique_ptr<vector<uint8_t>> loadFile(string const &fileName) {
-    ifstream file(fileName, ifstream::in | ifstream::binary);
-    if (file.fail()) {
-        cerr << "Can't open \"" << fileName << "\" for input" << endl;
-        return nullptr;
-    }
-
-    file.seekg(0, ifstream::end);
-    auto fileSize = file.tellg();
-    file.seekg(0);
-
-    auto data = make_unique<vector<uint8_t>>(fileSize);
-    file.read(reinterpret_cast<char *>(data->data()), data->size());
-    if (file.fail()) {
-        cerr << "Error reading \"" << fileName << "\"" << endl;
-        return nullptr;
-    }
-    return data;
-}
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    auto romData = loadFile("/Users/lk/mine/zed-80/src/zed-80/rom_only_test.rom");
-    if (romData == nullptr) {
-        NSLog(@"Can't load file");
-    } else {
-        _zed80.setRom(std::move(romData));
-    }
+
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
-}
-
-- (void)cancelEmulatorTimer {
-    if (_emulatorRunTimer != nil) {
-        [_emulatorRunTimer invalidate];
-        _emulatorRunTimer = nil;
-    }
-}
-
-- (IBAction)emulatorRun:(id)sender {
-    [self cancelEmulatorTimer];
-
-    uint32_t periodMs = 10;
-
-    _emulatorRunTimer = [NSTimer scheduledTimerWithTimeInterval:periodMs/1000.0 repeats:YES block:^(NSTimer * _Nonnull timer) {
-
-        self->_zed80.smallRun(periodMs);
-    }];
-}
-
-- (IBAction)emulatorStop:(id)sender {
-    [self cancelEmulatorTimer];
 }
 
 @end
--- emulator/gui/ViewController.mm
+++ emulator/gui/ViewController.mm
@@ -8,20 +8,94 @@
 
 #import "ViewController.h"
 #import "AppDelegate.h"
+#import "zed-80.hpp"
+#import <fstream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::unique_ptr;
+using std::make_unique;
+using std::make_shared;
+using std::vector;
+using std::string;
+using std::ifstream;
+
+@interface ViewController () {
+    ZED80 _zed80;
+    NSTimer *_emulatorRunTimer;
+}
+
+@end
 
 @implementation ViewController
+
+
+- (instancetype)init {
+    self = [super init];
+
+    _emulatorRunTimer = nil;
+
+    return self;
+}
+
+static unique_ptr<vector<uint8_t>> loadFile(string const &fileName) {
+    ifstream file(fileName, ifstream::in | ifstream::binary);
+    if (file.fail()) {
+        cerr << "Can't open \"" << fileName << "\" for input" << endl;
+        return nullptr;
+    }
+
+    file.seekg(0, ifstream::end);
+    auto fileSize = file.tellg();
+    file.seekg(0);
+
+    auto data = make_unique<vector<uint8_t>>(fileSize);
+    file.read(reinterpret_cast<char *>(data->data()), data->size());
+    if (file.fail()) {
+        cerr << "Error reading \"" << fileName << "\"" << endl;
+        return nullptr;
+    }
+    return data;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Do any additional setup after loading the view.
+    auto romData = loadFile("/Users/lk/mine/zed-80/src/zed-80/rom_only_test.rom");
+    if (romData == nullptr) {
+        NSLog(@"Can't load file");
+    } else {
+        _zed80.setRom(std::move(romData));
+    }
 }
-
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+- (void)cancelEmulatorTimer {
+    if (_emulatorRunTimer != nil) {
+        [_emulatorRunTimer invalidate];
+        _emulatorRunTimer = nil;
+    }
+}
+
+- (IBAction)emulatorRun:(id)sender {
+    [self cancelEmulatorTimer];
+
+    uint32_t periodMs = 10;
+
+    _emulatorRunTimer = [NSTimer scheduledTimerWithTimeInterval:periodMs/1000.0 repeats:YES block:^(NSTimer * _Nonnull timer) {
+
+        self->_zed80.smallRun(periodMs);
+    }];
+}
+
+- (IBAction)emulatorStop:(id)sender {
+    [self cancelEmulatorTimer];
 }
 
 @end
