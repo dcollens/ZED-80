84f7bec2cf667003c75443637619194813f0229f
Dan Collens <dan.collens@gmail.com>
2019-05-25 18:58:41 -0400

    Turn on LCD text cursor.

--- src/zed-80/lcd_test.asm
+++ src/zed-80/lcd_test.asm
@@ -170,6 +170,10 @@ lcd_test_text::
     M_lcdwrite0 LCDREG_CCR1
     M_lcdwrite0 LCDREG_FLDR	    ; vertical gap between lines, in pixels
     M_lcdwrite0 LCDREG_F2FSSR	    ; horizontal gap between characters, in pixels
+    M_lcdwrite LCDREG_GTCCR, 0x03   ; enable text cursor, turn on blink
+    M_lcdwrite LCDREG_BTCR, 14	    ; text cursor blink period: 15 frames
+    M_lcdwrite LCDREG_CURHS, LCD_TXT_WIDTH-1 ; cursor width equal to text width
+    M_lcdwrite LCDREG_CURVS, LCD_TXT_HEIGHT-1 ; cursor height equal to text height
     ld	    de, 0xFFFF
     ld	    hl, 0xFFFF
     call    lcd_set_fgcolor	    ; set FG color to white
