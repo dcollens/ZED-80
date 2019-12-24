1fbf42c7d82ffb54242f6d03a289095fd5080439
Dan Collens <dan.collens@gmail.com>
2019-12-23 20:50:58 -0500

    Add test programs to study power anomaly.

---
+++ src/zed-80/lcd_rdloop.asm
@@ -0,0 +1,61 @@
+; Calling convention used in this program
+; ---------------------------------------
+;
+; Unless otherwise noted, the first parameter, and the return value are stored as follows:
+; 8 bits: L
+; 16 bits: HL
+; 32 bits: DEHL
+;
+; Additional parameters are passed on the stack, left-to-right.
+; Parameters and return values larger than 32 bits are passed on the stack (return value
+; space set up by caller as a hidden first argument).
+; Callee saves/restores any modified registers.
+; Caller pops arguments after call returns.
+; AF registers are scratch (caller preserves, if needed).
+
+; same as 'rom', except that the default fill byte for 'defs' etc. is 0x00
+#target bin
+
+#include "z80.inc"
+#include "sysreg.inc"
+#include "lcd.inc"
+#include "7segdisp.inc"
+#include "joystick.inc"
+#include "z84c20.inc"
+#include "z84c30.inc"
+#include "z84c40.inc"
+#include "ascii.inc"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; our code will load immediately above the ROM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#code TEXT,0x4000
+
+; Careful, don't put anything before "init", as this is the entry point to our code.
+#local
+init::
+    ; zero the DATA segment
+    ld	    hl, DATA
+    ld	    bc, DATA_size
+    call    bzero
+
+    ; Set Sysreg to the value that we know the ROM monitor set it to.
+    ld	    a, SYS_MMUEN | SYS_SDCS | SYS_SDICLR
+    ld	    (Sysreg), a
+
+    call    seg_init		    ; clear 7-segment display
+loop:
+    in	    a, (PORT_LCDCMD)	    ; read LCD status byte
+    jr	    loop
+
+#endlocal
+
+#include library "libcode"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; data segment immediately follows code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#data DATA,TEXT_end
+; define static variables here
+
+#include library "libdata"
---
+++ src/zed-80/lcd_wrloop.asm
@@ -0,0 +1,72 @@
+; Calling convention used in this program
+; ---------------------------------------
+;
+; Unless otherwise noted, the first parameter, and the return value are stored as follows:
+; 8 bits: L
+; 16 bits: HL
+; 32 bits: DEHL
+;
+; Additional parameters are passed on the stack, left-to-right.
+; Parameters and return values larger than 32 bits are passed on the stack (return value
+; space set up by caller as a hidden first argument).
+; Callee saves/restores any modified registers.
+; Caller pops arguments after call returns.
+; AF registers are scratch (caller preserves, if needed).
+
+; same as 'rom', except that the default fill byte for 'defs' etc. is 0x00
+#target bin
+
+#include "z80.inc"
+#include "sysreg.inc"
+#include "lcd.inc"
+#include "7segdisp.inc"
+#include "joystick.inc"
+#include "z84c20.inc"
+#include "z84c30.inc"
+#include "z84c40.inc"
+#include "ascii.inc"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; our code will load immediately above the ROM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#code TEXT,0x4000
+
+; Careful, don't put anything before "init", as this is the entry point to our code.
+#local
+init::
+    ; zero the DATA segment
+    ld	    hl, DATA
+    ld	    bc, DATA_size
+    call    bzero
+
+    ; Set Sysreg to the value that we know the ROM monitor set it to.
+    ld	    a, SYS_MMUEN | SYS_SDCS | SYS_SDICLR
+    ld	    (Sysreg), a
+
+    call    seg_init		    ; clear 7-segment display
+    ;	RA8876_SW_Reset();
+    M_out   (PORT_LCDCMD), LCDREG_SRR
+    in	    a, (PORT_LCDDAT)
+    or	    0x01
+    out	    (PORT_LCDDAT), a
+wait_reset:
+    in	    a, (PORT_LCDDAT)
+    and	    0x01
+    jr	    nz, wait_reset
+
+    ld	    a, LCDREG_PPLLC1
+loop:
+    out	    (PORT_LCDCMD), a
+    jr	    loop
+
+#endlocal
+
+#include library "libcode"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; data segment immediately follows code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#data DATA,TEXT_end
+; define static variables here
+
+#include library "libdata"
