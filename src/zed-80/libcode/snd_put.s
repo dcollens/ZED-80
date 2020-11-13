385ec824fbc9d15bb1ab575275fe98e3c40f1d08
Dan Collens <dan.collens@gmail.com>
2020-11-12 23:07:23 -0500

    Optimize sound chip writing code.

--- src/zed-80/libcode/snd_put.s
+++ src/zed-80/libcode/snd_put.s
@@ -4,13 +4,13 @@
 ; - write 'data' to the audio chip using 'bus_cycle' (either SNDBUS_ADDR to set address register,
 ;   or SNDBUS_WRITE to write to the currently-addressed register)
 ; - pass 'bus_cycle' in L, and 'data' in H
+; - destroys: HL
 snd_put::
     ld	    a, h		    ; A = data
     out	    (PORT_PIOBDAT), a	    ; write 'data' to PIO port B
-    push    hl
-    ld	    h, ~(SYS_BDIR | SYS_BC1) ; clear bus control bits
-    call    sysreg_update	    ; set requested audio chip 'bus_cycle' bits
-    ld	    l, 0		    ; clear requested bus cycle bits
-    call    sysreg_update	    ; turn off audio chip bus control bits
-    pop	    hl
-    ret
+    ld	    a, (Sysreg)		    ; A = Sysreg
+    and	    a, ~(SYS_BDIR | SYS_BC1) ; clear bus control bits
+    or	    l			    ; set requested audo chip 'bus_cycle' bits
+    out	    (PORT_SYSREG), a
+    and	    a, ~(SYS_BDIR | SYS_BC1) ; clear bus control bits
+    jp	    sysreg_write	    ; update Sysreg variable and write to PORT_SYSREG
